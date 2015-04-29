34ab1f1a88c4ff33bced3cff024b7d538ae181e6
Malgorzata Nowicka <gosia.nowicka@uzh.ch>
2015-04-29 15:06:29 +0200

    Fig1: use gene sets v3; smaller font and brighter colours on heatmaps

--- RPlots_Figure1.R
+++ RPlots_Figure1.R
@@ -41,12 +41,48 @@ targets <- targets.org[keepSAMPS, ]
 ###########################################################################
 
 library(limma)
+library(ggplot2)
+
+
 labels <- paste0(ifelse(is.na(targets$ctrlRep), "", paste0(targets$ctrlRep, " ")), targets$labels)
 
 ### plot MDS for all samples
-pdf("PLOTS_Fig1/MDS.pdf")
-mds <- plotMDS(eset, top=1000, col = targets$colors, labels = labels, cex = 1.2, xlim = c(-2, 3))
+pdf("PLOTS_Fig1/MDS.pdf", width = 7, height = 7)
+mds <- plotMDS(eset, top=1000, col = targets$colors, labels = labels, cex = 1.5, xlim = c(-2, 3), ylim = c(-2, 3), las = 1, cex.axis = 1.5, cex.lab = 1.5)
 dev.off()
+
+
+
+pdf("PLOTS_Fig1/MDS.pdf", width = 5, height = 5)
+
+mds <- plotMDS(eset, top=1000, col = targets$colors, labels = labels, cex = 1, xlim = c(-2, 3), ylim = c(-2, 3), las = 1, cex.axis = 1, cex.lab = 1)
+
+dev.off()
+
+
+# df <- data.frame(x = mds$x, y = mds$y, labels = factor(labels), colors = factor(targets$colors))
+# 
+# 
+# ggp2 <- ggplot(df, aes(x = x, y = y, colour = colors)) +
+#   theme_bw() +
+#   geom_point(size = 2) +
+#   geom_text(aes(label=labels), hjust = 0.5, vjust = 0.5) +
+#   theme(legend.position="none")+
+#   theme(axis.text=element_text(size=12), axis.title=element_text(size=12,face="bold"), legend.title = element_text(size=12, face="bold"), legend.text = element_text(size = 12), panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+#   labs(x = "Leading logFC dim 1", y = "Leading logFC dim 2") +
+#   coord_cartesian(xlim = c(-2, 3), ylim = c(-2, 3)) +
+#   scale_colour_manual(values = levels(df$colors))
+# 
+# 
+# 
+# pdf("PLOTS_Fig1/MDSgg.pdf", width = 5, height = 5)
+# 
+# print(ggp2)
+# 
+# dev.off()
+
+
+
 
 ###########################################################################
 ### Heatmaps
@@ -64,100 +100,58 @@ eset <- eset[ ,ord]
 expr <- data.frame(exprs(eset))
 ### normalize expression per gene
 exprNorm <- t(scale(t(expr), center = TRUE, scale = TRUE))
-exprNorm2 <- t(scale(t(expr), center = TRUE, scale = FALSE))
-
 
 ### load list of genes of interest
 
-intrGenes <- read.table("Gene_Sets/niche_interaction_genes_v2_niche_binding.csv", header = TRUE, sep = ",")
+intrGenes <- read.table("Gene_Sets/niche_interaction_genes_v3_niche_binding.csv", header = TRUE, sep = ",")
+intrGenes <- read.table("Gene_Sets/niche_interaction_genes_v3_niche_formation.csv", header = TRUE, sep = ",")
 intrProbes <- as.character(intrGenes$ProbesetID)
 
 # dataHeat <- expr[intrProbes, ]
 dataHeat <- exprNorm[intrProbes, ]
 
+
+
 annotation_col <- targets[, "groups", drop = FALSE]
+colnames(annotation_col) <- "Samps"
 rownames(annotation_col) <- colnames(dataHeat)
 
 cols <- unique(targets$colors)
 names(cols) <- unique(targets$group)
   
 annotation_colors = list(groups = cols)
+names(annotation_colors) <- "Samps"
+
 
 labels_row <- strsplit2(intrGenes$GeneSymbol, " /// ")[, 1]
 
 
 
-pdf("PLOTS_Fig1/heatmap.pdf", width = 7, height = 10)
+pdf("PLOTS_Fig1/heatmap.pdf", width = 5.5, height = 5.5)
 
-pheatmap(dataHeat, cluster_cols = FALSE, annotation_col = annotation_col, annotation_colors = annotation_colors, labels_col = targets$groups, labels_row = labels_row, annotation_legend = FALSE, fontsize_row = 8, gaps_col = cumsum(table(targets$groups)), breaks =  seq(from = -3, to = 3, length.out = 101), legend_breaks = seq(from = -3, to = 3, by = 2), main = "Niche binding")
-
-dev.off()
-
-
-
-
-
-###########################################################################
-### Clustering expression data for all genes 
-###########################################################################
-
-## does not work 
-# resAll <- read.table("Comp1_DE_results_All.xls", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-
-allLines <- readLines("Comp1_DE_results_All.xls", n = -1)[-1]
-resAll <- data.frame(strsplit2(allLines, "\t"), stringsAsFactors = FALSE)
-colnames(resAll) <- strsplit2(readLines("Comp1_DE_results_All.xls", n = 1), "\t")
-
-resAll <- resAll[, !grepl(pattern = "CEL", colnames(resAll))]
-
-resAllSort <- resAll[order(resAll$CtrlCD4_PValues, resAll$CtrlCD4CD8_PValues, resAll$CtrlCD8_PValues, decreasing = FALSE), ]
-
-resAllSort$clusters <- apply(resAllSort[, c("CtrlCD4_Results", "CtrlCD4CD8_Results", "CtrlCD8_Results")], MARGIN = 1, paste, collapse = ",")
-
-resAllSort <- resAllSort[resAllSort$clusters != "0,0,0", ]
-
-
-library(gtools)
-
-clusters <- apply(permutations(n=3, r=3, v = c(-1, 0, 1), repeats.allowed=TRUE), MARGIN = 1, paste, collapse = ",")
-clusters <- clusters[clusters != "0,0,0"]
-
-resAllSort$clusters <- factor(resAllSort$clusters, levels = clusters)
-
-resAllSort <- resAllSort[order(resAllSort$clusters), ]
-
-unique(resAllSort$clusters)
-
-
-
-
-
-intrProbes <- as.character(resAllSort$ProbesetID)
-
-# dataHeat <- expr[intrProbes, ]
-dataHeat <- exprNorm[intrProbes, ]
-
-annotation_col <- targets[, "groups", drop = FALSE]
-rownames(annotation_col) <- colnames(dataHeat)
-
-cols <- unique(targets$colors)
-names(cols) <- unique(targets$group)
-
-annotation_colors = list(groups = cols)
-
-labels_row <- strsplit2(intrGenes$GeneSymbol, " /// ")[, 1]
-
-
-
-png("PLOTS_Fig1/heatmap_all.png", width = 700, height = 2000)
-
-pheatmap(dataHeat, cluster_cols = FALSE, cluster_rows = FALSE, annotation_col = annotation_col, annotation_colors = annotation_colors, labels_col = targets$groups, labels_row = rep("", nrow(dataHeat)), annotation_legend = FALSE, fontsize_row = 8, gaps_col = cumsum(table(targets$groups)), gaps_row = cumsum(table(resAllSort$clusters)),breaks =  seq(from = -4, to = 4, length.out = 101), legend_breaks = seq(from = -4, to = 4, by = 2))
+pheatmap(dataHeat, cluster_cols = FALSE, annotation_col = annotation_col, annotation_colors = annotation_colors, labels_col = targets$groups, labels_row = labels_row, annotation_legend = FALSE, fontsize_row = 6, fontsize_col = 6, gaps_col = cumsum(table(targets$groups)), breaks =  seq(from = -3, to = 3, length.out = 101), legend_breaks = seq(from = -3, to = 3, by = 2), main = "Niche binding", cellheight = 7, cellwidth = 14, fontsize = 7)
 
 dev.off()
 
 
 
-write.table(resAllSort, file = "Comp1_DEclusters.xls", quote = FALSE, sep = "\t", row.names = FALSE)
+library(RColorBrewer)
+library(colorRamps)
+
+pdf("PLOTS_Fig1/heatmap.pdf", width = 5, height = 5)
+
+pheatmap(dataHeat, color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(100), cluster_cols = FALSE, annotation_col = annotation_col, annotation_colors = annotation_colors, labels_col = targets$groups, labels_row = labels_row, annotation_legend = FALSE, fontsize_row = 6, fontsize_col = 6, gaps_col = cumsum(table(targets$groups)), breaks =  seq(from = -3, to = 3, length.out = 101), legend_breaks = seq(from = -3, to = 3, by = 2), main = "Niche binding", fontsize = 7)
+
+pheatmap(dataHeat, color = colorRamps::matlab.like(100), cluster_cols = FALSE, annotation_col = annotation_col, annotation_colors = annotation_colors, labels_col = targets$groups, labels_row = labels_row, annotation_legend = FALSE, fontsize_row = 6, fontsize_col = 6, gaps_col = cumsum(table(targets$groups)), breaks =  seq(from = -3, to = 3, length.out = 101), legend_breaks = seq(from = -3, to = 3, by = 2), main = "Niche binding", fontsize = 7)
+
+
+pheatmap(dataHeat, color = colorRamps::matlab.like2(100), cluster_cols = FALSE, annotation_col = annotation_col, annotation_colors = annotation_colors, labels_col = targets$groups, labels_row = labels_row, annotation_legend = FALSE, fontsize_row = 6, fontsize_col = 6, gaps_col = cumsum(table(targets$groups)), breaks =  seq(from = -3, to = 3, length.out = 101), legend_breaks = seq(from = -3, to = 3, by = 2), main = "Niche binding", fontsize = 7)
+
+
+
+dev.off()
+
+
 
 
 
--- Ranalysis_Mouse.R
+++ Ranalysis_Mouse.R
@@ -19,6 +19,8 @@
 # - add pre VS after treatment analysis
 # 17 Apr 2015
 # - add barcodeplots in the gene set analysis 
+# 29 Apr 2015
+# - clustering + GO on clusters
 
 ###########################################################################
 
@@ -1596,6 +1598,69 @@ write.table(gseaAll, paste("Comp1_GSEA_Hallmark_All.xls", sep=""), sep="\t", row
 
 # http://www.broadinstitute.org/gsea/doc/GSEAUserGuideTEXT.htm
 
+
+
+###########################################################################
+### Clustering for all genes based on DE results (-1, 0, 1)
+###########################################################################
+
+## does not work 
+# resAll <- read.table("Comp1_DE_results_All.xls", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
+allLines <- readLines("Comp1_DE_results_All.xls", n = -1)[-1]
+resAll <- data.frame(strsplit2(allLines, "\t"), stringsAsFactors = FALSE)
+colnames(resAll) <- strsplit2(readLines("Comp1_DE_results_All.xls", n = 1), "\t")
+
+resAll <- resAll[, !grepl(pattern = "CEL", colnames(resAll))]
+
+resAllSort <- resAll[order(resAll$CtrlCD4_PValues, resAll$CtrlCD4CD8_PValues, resAll$CtrlCD8_PValues, decreasing = FALSE), ]
+
+resAllSort$clusters <- apply(resAllSort[, c("CtrlCD4_Results", "CtrlCD4CD8_Results", "CtrlCD8_Results")], MARGIN = 1, paste, collapse = ",")
+
+resAllSort <- resAllSort[resAllSort$clusters != "0,0,0", ]
+
+
+library(gtools)
+
+clusters <- apply(permutations(n=3, r=3, v = c(-1, 0, 1), repeats.allowed=TRUE), MARGIN = 1, paste, collapse = ",")
+clusters <- clusters[clusters != "0,0,0"]
+
+resAllSort$clusters <- factor(resAllSort$clusters, levels = clusters)
+
+resAllSort <- resAllSort[order(resAllSort$clusters), ]
+
+unique(resAllSort$clusters)
+
+
+
+
+
+intrProbes <- as.character(resAllSort$ProbesetID)
+
+# dataHeat <- expr[intrProbes, ]
+dataHeat <- exprNorm[intrProbes, ]
+
+annotation_col <- targets[, "groups", drop = FALSE]
+rownames(annotation_col) <- colnames(dataHeat)
+
+cols <- unique(targets$colors)
+names(cols) <- unique(targets$group)
+
+annotation_colors = list(groups = cols)
+
+labels_row <- strsplit2(intrGenes$GeneSymbol, " /// ")[, 1]
+
+
+
+png("PLOTS_Fig1/heatmap_all.png", width = 700, height = 2000)
+
+pheatmap(dataHeat, cluster_cols = FALSE, cluster_rows = FALSE, annotation_col = annotation_col, annotation_colors = annotation_colors, labels_col = targets$groups, labels_row = rep("", nrow(dataHeat)), annotation_legend = FALSE, fontsize_row = 8, gaps_col = cumsum(table(targets$groups)), gaps_row = cumsum(table(resAllSort$clusters)),breaks =  seq(from = -4, to = 4, length.out = 101), legend_breaks = seq(from = -4, to = 4, by = 2))
+
+dev.off()
+
+
+
+write.table(resAllSort, file = "Comp1_DEclusters.xls", quote = FALSE, sep = "\t", row.names = FALSE)
 
 
 
